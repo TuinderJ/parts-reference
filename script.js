bbd679a6ca654373d397f0064b0a95d8f0f0a665
tuinderj <joshua.tuinder@gmail.com>
2025-07-05 13:49:52 -0600

    relay fix

--- script.js
+++ script.js
@@ -201,6 +201,7 @@ function createPartsSearchList(searchTerm) {
     for (group in data.categories[category].groups) {
       data.categories[category].groups[group].forEach((part) => {
         const li = document.createElement('li');
+        console.log(part);
         if (
           !part.description.toLowerCase().includes(searchTerm.toLowerCase()) &&
           !convert.toTextCase(group).toLowerCase().includes(convert.toTextCase(searchTerm).toLowerCase()) &&
